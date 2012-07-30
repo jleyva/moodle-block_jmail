173b296774d6b3453926e19bf7552a90d7d5268a
Juan Leyva <jleyva@cvaconsulting.com>
2012-07-30 12:45:50 +0200

    Added spanish language and reformated README

---
+++ lang/es/block_jmail.php
@@ -0,0 +1,107 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings translations.
+ *
+ * @package    blocks
+ * @subpackage jmail
+ * @copyright  2011 Juan Leyva <juanleyvadelgado@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['addlabel'] = 'Añadir etiqueta';
+$string['approvalpending'] = 'Aprobación pendiente';
+$string['approve'] = 'Aprobar';
+$string['approvemode'] = 'Modo de aprobación';
+$string['Adunto'] = 'Adunto';
+$string['Aduntos'] = 'Aduntos';
+$string['bcc'] = 'BCC';
+$string['bin'] = 'Papelera';
+$string['body'] = 'Cuerpo';
+$string['cc'] = 'CC';
+$string['check'] = 'Comprobar';
+$string['compose'] = 'Nuevo';
+$string['configenableglobalinbox'] = '¿Los usuarios deberían ver la bandeja de correo global? Esto implica que verán como contactos a todos los usuarios del sistema';
+$string['confirmdelete'] = '¿Estás seguro que deseas borrar el/los mensaje/s seleccionado/s?';
+$string['contacts'] = 'Contactos';
+$string['date'] = 'Fecha';
+$string['delete'] = 'Borrar';
+$string['deletem'] = 'Borrar';
+$string['delivertodifferentcourse'] = 'Tu mensaje ha sido enviado a la bandeja de correo indicada del usuario:';
+$string['delivertoglobalinbox'] = 'Tu mensaje ha sido enviado a la bandeja de correo global del usuario';
+$string['download'] = 'Descargar';
+$string['draft'] = 'Borrador';
+$string['enableglobalinbox'] = 'Habilitar bandeja de correo global';
+$string['emailcopyheader'] = 'Esto es una copia de un mensaje de correo que has recibido en una de tus bandejas de correo.<br /> Puedes ver el mensaje al completo aquí: {$a}';
+$string['emailcopyfooter'] = '';
+$string['errorsubjectrequired'] = 'El campo asunto es obligatorio';
+$string['errortorequired'] = 'Debes indicar al menos un destinatario';
+$string['filesaved'] = 'Archivo guardado';
+$string['filterfield'] = 'Campo para filtrar usuarios';
+$string['first'] = '<< Primero';
+$string['forward'] = 'Siguiente';
+$string['from'] = 'De';
+$string['fw'] = 'Fw:';
+$string['inbox']= 'Inbox';
+$string['label'] = 'Etiqueta';
+$string['labels'] = 'Etiquetas';
+$string['last'] = 'Último >>';
+$string['mailbox'] = 'Bandeja';
+$string['mailboxs'] = 'Bandejas';
+$string['markas']= 'Marcar como';
+$string['markread']= 'Marcar leído';
+$string['markunread']= 'Marcar no leído';
+$string['messagehastobeapproved']= 'Tu mensaje no será entregado a los destinatarios hasta que un moderador lo apruebe';
+$string['messagesent']= 'Mensaje enviado';
+$string['messagesaved']= 'Mensaje guardado';
+$string['messagedeleted']= 'Mensaje borrado';
+$string['messagesdeleted']= 'Mensajes borrados';
+$string['mymailboxes'] = 'Mis buzones';
+$string['more'] = 'Más';
+$string['move'] = 'Mover';
+$string['next'] = 'Siguiente >>';
+$string['newmail'] = 'Nuevo';
+$string['none'] = 'Ninguno';
+$string['nomessagesfound'] = 'No se encontraron mensajes';
+$string['nonewmessages'] = 'No hay mensajes nuevos';
+$string['toapprove'] = 'Para aprobar';
+$string['preferences'] = 'Preferencias';
+$string['previous'] = '<< Anteriores';
+$string['pluginname'] = 'jmail';
+$string['print'] = 'Imprimir';
+$string['re'] = 'Re:';
+$string['receivecopies'] = 'Recivir copias de los mensajes en mi cuenta de correo personal';
+$string['reply'] = 'Contestar';
+$string['replyall'] = 'Contestar a todos';
+$string['save'] = 'Guardar';
+$string['savetomyprivatefiles'] = 'Guardar a mis archivos privados';
+$string['selected'] = 'Seleccionados';
+$string['send']= 'Enviar';
+$string['sent']= 'Enviado';
+$string['subject'] = 'Asunto';
+$string['subscription'] = 'Suscripción';
+$string['to']= 'Para';
+$string['unreadmessages']= '{$a} mensajes no leídos';
+$string['viewall']= 'Ver todos';
+$string['viewglobalinbox']= 'Ver bandeja de correo global';
+$string['viewinbox']= 'Ver correo';
+$string['jmail:approvemessages'] = 'Aprobar mensajes';
+$string['jmail:managelabels'] = 'Gestionar etiquetas';
+$string['jmail:managepreferences'] = 'Gestionar preferencias';
+$string['jmail:sendtoall'] = 'Enviar mensajes a todos';
+$string['jmail:sendtomanagers'] = 'Enviar mensajes a gestores';
+$string['jmail:viewmailbox'] = 'Ver bandeja de correo';
