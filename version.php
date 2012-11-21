d3f6db6138cb3c551c0fd0ad2b071c0c578c6880
Juan Leyva <juanleyvadelgado@gmail.com>
2012-11-21 10:21:04 +0100

    Moodle 2.1 at least required

--- version.php
+++ version.php
@@ -26,6 +26,6 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version  = 2011122505;
-$plugin->requires = 2010112400;
+$plugin->requires = 2011070100;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = 1.4;
