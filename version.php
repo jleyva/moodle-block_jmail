897c094e03a286deb8098e0af60c8c80d9af1f82
Juan Leyva <juanleyvadelgado@gmail.com>
2013-12-18 09:39:21 +0100

    Changing minimal version required

--- version.php
+++ version.php
@@ -26,6 +26,6 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version  = 2011122510;
-$plugin->requires = 2011070100;
+$plugin->requires = 2011120500;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = 1.7;
